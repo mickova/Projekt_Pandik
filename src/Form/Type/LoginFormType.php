7c432725c1f5aa413cbed95c27b8976af116059e
MLorenz04 <matyaslorenz@seznam.cz>
2023-02-12 22:44:00 +0100

    Opravení email inputu u přihlášení a registrace

--- src/Form/Type/LoginFormType.php
+++ src/Form/Type/LoginFormType.php
@@ -4,14 +4,14 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 class LoginFormType extends AbstractType
 {
    public function buildForm(FormBuilderInterface $builder, array $options)
    {
       $builder
-         ->add('email', TextType::class)
+         ->add('email', EmailType::class)
          ->add('password', PasswordType::class);
    }
 }
