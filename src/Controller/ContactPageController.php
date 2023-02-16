459741d7990cde719973ea50cbec6e55fca99fbe
MLorenz04 <matyaslorenz@seznam.cz>
2023-02-16 16:28:09 +0100

    Verze 1.0 kontaktů

--- src/Controller/ContactPageController.php
+++ src/Controller/ContactPageController.php
@@ -3,23 +3,41 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\Type\LoginFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\SecurityBundle\Security;
+use App\Form\Type\ContactFormType;
+use App\Entity\User;
 
 class ContactPageController extends AbstractController
 {
    #[Route('/contact', name: "contact", methods: ['GET'])]
-   public function contact(Security $security)
+   public function contact(Security $security, Request $request, ManagerRegistry $doctrine)
    {
-      $user = $security->getUser();
-      if($user->getId() != null) {
-      return $this->render('html/contact.html.twig', [
-         "logged"=> true
-      ]);
-   } else {
-      return $this->render('html/contact.html.twig', [
-         "logged"=> false
-      ]);
-   }
+      $form = $this->createForm(ContactFormType::class);
+      $form->handleRequest($request);
+
+      if ($form->isSubmitted() && $form->isValid()) {
+         $msg = $form->getData()["message"];
+
+         // use wordwrap() if lines are longer than 70 characters
+         $msg = wordwrap($msg, 70);
+
+         // send email
+         mail("matyaslorenz@seznam.cz", $form->getData()["email"], $msg);
+      }
+      if ($security->getUser() != null) {
+         return $this->render('html/contact.html.twig', [
+            "logged" => true,
+            "ContactForm" => $form->createView()
+         ]);
+      } else {
+         return $this->render('html/contact.html.twig', [
+            "logged" => false,
+            "ContactForm" => $form->createView()
+         ]);
+      }
    }
 }
